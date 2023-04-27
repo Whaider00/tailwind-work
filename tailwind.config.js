9aa49550a10ca3640e4ad77bd5c6e61a9124e9ab
Wajih Haider <wajihhaider00@gmail.com>
2023-04-27 17:59:38 +0500

    Add changes and delete files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         header: "#151515",
         primary: "#008000",
         secondary: "#968888",
+        headercolor: "#f63232",
+        primarycolor: "#feb614",
+        secondarycolor: "#f4f4f4",
+        Textcolor: "#4b4b4b"
       }
     },
   },
